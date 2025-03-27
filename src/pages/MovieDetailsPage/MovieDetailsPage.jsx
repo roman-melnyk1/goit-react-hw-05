54cd3c144eb9de5abcb110e8499d2d51b1c41666
iyrd <chilromko@gmail.com>
2025-03-27 19:09:52 +0100

    dddone

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,5 +1,4 @@
-// src/pages/MovieDetailsPage/MovieDetailsPage.jsx
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useParams, Link, Outlet, useLocation } from "react-router-dom";
 import { getMovieDetails } from "../../services/api";
 
@@ -7,7 +6,8 @@ const MovieDetailsPage = () => {
   const { movieId } = useParams();
   const [movie, setMovie] = useState(null);
   const location = useLocation();
-  const backLink = location.state?.from || "/movies";
+  
+  const backLinkRef = useRef(location.state?.from || "/movies");
 
   useEffect(() => {
     getMovieDetails(movieId).then(setMovie);
@@ -17,7 +17,7 @@ const MovieDetailsPage = () => {
 
   return (
     <div>
-      <Link to={backLink}>Go back</Link>
+      <Link to={backLinkRef.current}>Go back</Link>
       <h1>{movie.title}</h1>
       <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
       <p>{movie.overview}</p>
@@ -25,7 +25,6 @@ const MovieDetailsPage = () => {
         <Link to="cast">Cast</Link>
         <Link to="reviews">Reviews</Link>
       </nav>
-      {/* Outlet для рендерингу вкладених маршрутів */}
       <Outlet />
     </div>
   );
